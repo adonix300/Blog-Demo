11e721da02c542b5ce1e849a661647615c5aa390
adonix300 <businessskyjoyboy@gmail.com>
2024-05-16 07:51:14 +0500

    Integrations tests

---
+++ src/test/java/abdulgazizov/dev/blogdemo/controllers/AuthControllerIT.java
@@ -0,0 +1,145 @@
+package abdulgazizov.dev.blogdemo.controllers;
+
+import abdulgazizov.dev.blogdemo.TestContainerConfig;
+import abdulgazizov.dev.blogdemo.models.dto.AuthRequest;
+import abdulgazizov.dev.blogdemo.services.impl.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = TestContainerConfig.class)
+@TestPropertySource(locations = "classpath:application-test.yaml")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+class AuthControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private AuthRequest authRequest;
+
+    @BeforeEach
+    void setUp() {
+        authRequest = new AuthRequest();
+        authRequest.setUsername("user");
+        authRequest.setPassword("password");
+    }
+
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.execute("TRUNCATE TABLE comments CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE posts CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE users CASCADE");
+    }
+
+    @Test
+    public void signUp_testSuccess() throws Exception {
+        mockMvc.perform(post("/auth/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.token").isNotEmpty());
+    }
+
+    @Test
+    public void signIn_testSuccess() throws Exception {
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").isNotEmpty());
+    }
+
+    @Test
+    public void signUp_testWithExistingUsername() throws Exception {
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void signIn_testWithWrongPassword() throws Exception {
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isCreated());
+
+        AuthRequest wrongPasswordRequest = new AuthRequest();
+        wrongPasswordRequest.setUsername(authRequest.getUsername());
+        wrongPasswordRequest.setPassword("wrongpassword");
+
+        mockMvc.perform(post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(wrongPasswordRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void signIn_testWithNonExistentUser() throws Exception {
+        AuthRequest nonExistentUserRequest = new AuthRequest();
+        nonExistentUserRequest.setUsername("nonexistentuser");
+        nonExistentUserRequest.setPassword("password");
+
+        mockMvc.perform(post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(nonExistentUserRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void signUp_testWithEmptyUsername() throws Exception {
+        AuthRequest emptyUsernameRequest = new AuthRequest();
+        emptyUsernameRequest.setUsername("");
+        emptyUsernameRequest.setPassword("password");
+
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(emptyUsernameRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void signUp_testWithEmptyPassword() throws Exception {
+        AuthRequest emptyPasswordRequest = new AuthRequest();
+        emptyPasswordRequest.setUsername("user_" + UUID.randomUUID());
+        emptyPasswordRequest.setPassword("");
+
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(emptyPasswordRequest)))
+                .andExpect(status().isBadRequest());
+    }
+}
---
+++ src/test/java/abdulgazizov/dev/blogdemo/controllers/PostControllerIT.java
@@ -0,0 +1,293 @@
+package abdulgazizov.dev.blogdemo.controllers;
+
+import abdulgazizov.dev.blogdemo.TestContainerConfig;
+import abdulgazizov.dev.blogdemo.exceptions.UserNotFoundException;
+import abdulgazizov.dev.blogdemo.models.dto.PostDto;
+import abdulgazizov.dev.blogdemo.models.entities.UserEntity;
+import abdulgazizov.dev.blogdemo.models.user.CustomUserDetails;
+import abdulgazizov.dev.blogdemo.repositories.UserRepository;
+import abdulgazizov.dev.blogdemo.secutiry.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = TestContainerConfig.class)
+@TestPropertySource(locations = "classpath:application-test.yaml")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+class PostControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private PostDto postDto;
+
+    private String token;
+
+    private String signupUser(String username, String password) {
+        password = passwordEncoder.encode(password);
+        String sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, username, password, "ROLE_USER");
+
+        UserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("wrong " + username));
+
+        UserDetails userDetails = new CustomUserDetails(userEntity);
+
+        return jwtService.generateToken(userDetails);
+//        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//        SecurityContextHolder.getContext().setAuthentication(authRequest);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        postDto = PostDto.builder()
+                .title("Test Title")
+                .content("Test Content")
+                .build();
+
+        token = signupUser("test_user", "test_password");
+    }
+
+    @AfterEach
+    void tearDown() {
+        clearDataBase();
+        SecurityContextHolder.clearContext();
+    }
+
+    private void clearDataBase() {
+        jdbcTemplate.execute("TRUNCATE TABLE comments CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE posts CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE users CASCADE");
+    }
+
+    @Test
+    public void createPost_testSuccess() throws Exception {
+        mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title", containsString("Test Title")))
+                .andExpect(jsonPath("$.content", containsString("Test Content")));
+    }
+
+    @Test
+    public void createPost_testForbidden() throws Exception {
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void createPost_testValidationFailure() throws Exception {
+        PostDto postDto = PostDto.builder()
+                .title("")
+                .content("Test Content")
+                .build();
+
+        mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void getAllPosts_testSuccess() throws Exception {
+        mockMvc.perform(get("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void getAllPosts_testForbidden() throws Exception {
+        mockMvc.perform(get("/posts")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void getPostById_testSuccess() throws Exception {
+        String postResponse = mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Long postId = JsonPath.parse(postResponse).read("$.id", Long.class);
+
+        mockMvc.perform(get("/posts/{id}", postId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", containsString("Test Title")))
+                .andExpect(jsonPath("$.content", containsString("Test Content")));
+    }
+
+    @Test
+    public void getPostById_testNotFound() throws Exception {
+        mockMvc.perform(get("/posts/{id}", 999L)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void updatePost_testForbidden() throws Exception {
+        String postResponse = mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Long postId = JsonPath.parse(postResponse).read("$.id", Long.class);
+
+        String token2 = signupUser("new_user", "new_password");
+
+        PostDto updatedPostDto = PostDto.builder()
+                .title("Updated Test Title")
+                .content("Updated Test Content")
+                .build();
+
+        mockMvc.perform(put("/posts/{id}", postId)
+                        .header("Authorization", "Bearer " + token2)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedPostDto)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void updatePost_testSuccess() throws Exception {
+        String postResponse = mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Long postId = JsonPath.parse(postResponse).read("$.id", Long.class);
+
+        PostDto updatedPostDto = PostDto.builder()
+                .title("Updated Test Title")
+                .content("Updated Test Content")
+                .build();
+
+        mockMvc.perform(put("/posts/{id}", postId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedPostDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", containsString("Updated Test Title")))
+                .andExpect(jsonPath("$.content", containsString("Updated Test Content")));
+    }
+
+    @Test
+    public void updatePost_testNotFound() throws Exception {
+        PostDto updatedPostDto = PostDto.builder()
+                .title("Updated Test Title")
+                .content("Updated Test Content")
+                .build();
+
+        mockMvc.perform(put("/posts/{id}", 999L)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedPostDto)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deletePost_testSuccess() throws Exception {
+        String postResponse = mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Long postId = JsonPath.parse(postResponse).read("$.id", Long.class);
+
+        mockMvc.perform(delete("/posts/{id}", postId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Successfully deleted post"));
+
+        mockMvc.perform(get("/posts/{id}", postId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deletePost_testForbidden() throws Exception {
+        PostDto postDto = PostDto.builder()
+                .title("Test Title")
+                .content("Test Content")
+                .build();
+
+        String postResponse = mockMvc.perform(post("/posts")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Long postId = JsonPath.parse(postResponse).read("$.id", Long.class);
+
+        SecurityContextHolder.clearContext();
+        String token2 = signupUser("new_user", "new_password");
+        mockMvc.perform(delete("/posts/{id}", postId)
+                        .header("Authorization", "Bearer " + token2)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void deletePost_testNotFound() throws Exception {
+        mockMvc.perform(delete("/posts/{id}", 999L)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
